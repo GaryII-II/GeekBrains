ce04c611d39b5b9e998ba6264f4572170ba0d6a2
gary.ivanov <gary.ivanov@lge.com>
2021-05-08 12:11:52 +0300

    Added interim changes

---
+++ PythonBasics/Lesson2/lesson2_1.py
@@ -0,0 +1,12 @@
+﻿# Artificial Intelligence
+# Igor Ivanov
+# Lesson 2. Task 1
+
+# List of different data types
+
+data_types = [1, 'two', 3.3, complex(2,3), True, ('one', 'two'), {'title': 'Python lesson', 'version': '3.8'}, {23, 34},
+              [111, '111']]
+
+for index, item in enumerate(data_types):
+    print(' Item {0} with type {1} has value {2}'.format(index, type(item), item))
+
