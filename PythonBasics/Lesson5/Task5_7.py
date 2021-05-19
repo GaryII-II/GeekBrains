dbf2634bd5a71f9c8344205de15fc5b051954404
Gary Ivanov <gary.ivanov@lge.com>
2021-05-19 23:03:55 +0300

    Added lesson5

---
+++ PythonBasics/Lesson5/Task5_7.py
@@ -0,0 +1,12 @@
+# Artificial Intelligence
+# Igor Ivanov
+# Lesson 5
+# Task 7. Read firms info, calculate profit data
+
+from functools import reduce
+
+# Format: Alfa OOO 10000 5000.
+print('Read task7_in.txt')
+
+# TBD very soon
+
