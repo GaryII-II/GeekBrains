839b89538306d110caf38fcca35b314ac3c8c6af
gary.ivanov <gary.ivanov@lge.com>
2021-05-21 18:52:55 +0300

    Added postponed task7

--- PythonBasics/Lesson5/Task5_7.py
+++ PythonBasics/Lesson5/Task5_7.py
@@ -3,10 +3,33 @@
 # Lesson 5
 # Task 7. Read firms info, calculate profit data
 
-from functools import reduce
+from sys import exc_info
+from json import dump
 
 # Format: Alfa OOO 10000 5000.
 print('Read task7_in.txt')
 
-# TBD very soon
+firms = {}
+profit = 0
+profitables = 0
+
+try:
+    with open('task7_in.txt', 'r', encoding='utf-8-sig') as in_file:
+        for line in in_file:
+            values = line.rstrip('.\n').split()
+            diff = int(values[2]) - int(values[3])
+            firms.update({values[0]: diff})
+            if diff > 0:
+                profit += diff
+                profitables += 1
+
+    avg_profit = 0 if profitables < 1 else profit / profitables
+
+    with open('task7_out.txt', 'w') as out_file:
+        dump([firms, {'average_profit': avg_profit}], out_file)
+
+except:
+    e = sys.exc_info()
+    print('Error: ', e)
+    quit()
 
